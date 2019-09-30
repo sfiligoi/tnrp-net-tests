9696d95e053e9f2b8231331a98a5ec542249a06b
Igor Sfiligoi <isfiligoi@ucsd.edu>
2019-09-30 13:06:16 -0700

    Add multi-destination Azure East multi raw results

---
+++ raw_data_multi/msae1_i2/aria1NYSD_32/aria1.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+let d=$1%2
+
+if [ $d -eq 0 ]; then
+  dn=stashcache.t2.ucsd.edu:8000
+else
+  dn=osg.newy32aoa.nrp.internet2.edu:9001
+fi
+
+cd /dev/shm
+pwd
+i=`echo ${_CONDOR_SLOT} | awk '{split($0,a,"slot"); print a[2]}'`
+echo Slot ${_CONDOR_SLOT}
+echo "Fetching 1G_$i.dat (http://${dn}/user/dweitzel/public/chep/file.cvmfs.1000)"
+rm -f 1G_$i.*dat
+date
+date 1>&2
+time aria2c --header='Want-Digest: ' -q -j 80 -x 16 http://${dn}/user/dweitzel/public/chep/file.cvmfs.1000 -o 1G_$i.dat
+echo "RC $?"
+ls -l 1G_$i.dat
+rm -f 1G_$i.*dat
+echo "--- Mid run ---"
+echo "--- Mid run ---" 1>&2
+date
+date 1>&2
+time aria2c --header='Want-Digest: ' -q -j 80 -x 16 http://${dn}/user/dweitzel/public/chep/file.cvmfs.1000 -o 1G_$i.dat
+echo "RC $?"
+ls -l 1G_$i.dat
+rm -f 1G_$i.*dat
+echo "=== Mid run end ==="
+echo "=== Mid run end ===" 1>&2
+date
+date 1>&2
+time aria2c -q --header='Want-Digest: ' -j 80 -x 16 http://${dn}/user/dweitzel/public/chep/file.cvmfs.1000 -o 1G_$i.dat
+echo "RC $?"
+ls -l 1G_$i.dat
+rm -f 1G_$i.*dat
+date
+date 1>&2
+time aria2c --header='Want-Digest: ' -q -j 80 -x 16 http://${dn}/user/dweitzel/public/chep/file.cvmfs.1000 -o 1G_$i.dat
+echo "RC $?"
+ls -l 1G_$i.dat
+rm -f 1G_$i.*dat
+date
+date 1>&2
+time aria2c --header='Want-Digest: ' -q -j 80 -x 16 http://${dn}/user/dweitzel/public/chep/file.cvmfs.1000 -o 1G_$i.dat
+echo "RC $?"
+ls -l 1G_$i.dat
+rm -f 1G_$i.*dat
+date
+date 1>&2
+time aria2c --header='Want-Digest: ' -q -j 80 -x 16 http://${dn}/user/dweitzel/public/chep/file.cvmfs.1000 -o 1G_$i.dat
+echo "RC $?"
+ls -l 1G_$i.dat
+rm -f 1G_$i.*dat
+date
+date 1>&2
+
