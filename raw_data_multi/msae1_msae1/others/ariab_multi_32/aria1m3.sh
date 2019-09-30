9c1d10098db6ec41530735c8b6ec3f62ea34fad9
Igor Sfiligoi <isfiligoi@ucsd.edu>
2019-09-30 16:58:53 -0700

    Add Azure East to Blob raw results

---
+++ raw_data_multi/msae1_msae1/others/ariab_multi_32/aria1m3.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+cd /dev/shm
+pwd
+let i=$1/3
+b=`echo $i | awk '{a=$1%16; printf("%x\n",a)}'`
+let m=$1%3
+if [ $m -eq 0 ]; then
+  mc=m
+elif [ $m -eq 1 ]; then
+  mc=a
+else
+  mc=b
+fi
+
+date
+date 1>&2
+echo Job $i
+echo Fetching http://exaeastb0.blob.core.windows.net/exa-east/${b}_test${mc}/data/1G_$i.dat
+rm -f 1G_$i.*dat
+date
+date 1>&2
+time aria2c --retry-wait=1 -j 80 -x 16 -q http://exaeastb0.blob.core.windows.net/exa-east/${b}_test${mc}/data/1G_$i.dat
+echo "RC $?"
+ls -l 1G_$i.dat
+rm -f 1G_$i.*dat
+date
+date 1>&2
+time aria2c --retry-wait=1 -j 80 -x 16 -q http://exaeastb0.blob.core.windows.net/exa-east/${b}_test${mc}/data/1G_$i.dat
+echo "RC $?"
+ls -l 1G_$i.dat
+rm -f 1G_$i.*dat
+date
+date 1>&2
+time aria2c --retry-wait=1 -j 80 -x 16 -q http://exaeastb0.blob.core.windows.net/exa-east/${b}_test${mc}/data/1G_$i.dat
+echo "RC $?"
+ls -l 1G_$i.dat
+rm -f 1G_$i.*dat
+echo "--- Mid run ---"
+echo "--- Mid run ---" 1>&2
+date
+date 1>&2
+time aria2c --retry-wait=1 -j 80 -x 16 -q http://exaeastb0.blob.core.windows.net/exa-east/${b}_test${mc}/data/1G_$i.dat
+echo "RC $?"
+ls -l 1G_$i.dat
+rm -f 1G_$i.*dat
+echo "=== Mid run end ==="
+echo "=== Mid run end ===" 1>&2
+date
+date 1>&2
+time aria2c --retry-wait=1 -j 80 -x 16 -q http://exaeastb0.blob.core.windows.net/exa-east/${b}_test${mc}/data/1G_$i.dat
+echo "RC $?"
+ls -l 1G_$i.dat
+rm -f 1G_$i.*dat
+date
+date 1>&2
+time aria2c --retry-wait=1 -j 80 -x 16 -q http://exaeastb0.blob.core.windows.net/exa-east/${b}_test${mc}/data/1G_$i.dat
+echo "RC $?"
+ls -l 1G_$i.dat
+rm -f 1G_$i.*dat
+date
+date 1>&2
+
