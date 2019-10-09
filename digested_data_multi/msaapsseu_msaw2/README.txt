3d22bb2c1f7a2c1331811afed370b4ae98e8fbe8
Igor Sfiligoi <isfiligoi@ucsd.edu>
2019-10-08 17:56:31 -0700

    Add Azure AP to USW BLOB transfers

---
+++ digested_data_multi/msaapsseu_msaw2/README.txt
@@ -0,0 +1,2 @@
+# commands used to create the digested files
+for d in ariapmtW_32 ariapmtW_160 ariapmtW_400  ; do echo $d; mkdir $d; (cd $d && ../../digest_one.py ../../../raw_data_multi/msaapsseu_msaw2/$d); done
---
+++ raw_data_multi/msaapsseu_msaw2/ariapmtW_160/aria1m3.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+cd /dev/shm
+mkdir -p "user_${_CONDOR_SLOT}"
+cd "user_${_CONDOR_SLOT}"
+pwd
+let i=$1/3
+b=`echo $i | awk '{a=$1%16; printf("%x\n",a)}'`
+let m=$1%3
+if [ $m -eq 0 ]; then
+  mc=m
+elif [ $m -eq 1 ]; then
+  mc=a
+else
+  mc=b
+fi
+
+date
+date 1>&2
+echo Job $i
+echo Fetching https://exawestp${b}.blob.core.windows.net/exa-west/test${mc}/data/1G_$i.dat
+rm -f 1G_$i.*dat
+date
+date 1>&2
+time aria2c --max-tries=32 --retry-wait=1  -q -j 80 -x 16 https://exawestp${b}.blob.core.windows.net/exa-west/test${mc}/data/1G_$i.dat
+echo "RC $?"
+ls -l 1G_$i.dat
+rm -f 1G_$i.*dat
+echo "--- Mid run ---"
+echo "--- Mid run ---" 1>&2
+date
+date 1>&2
+time aria2c --max-tries=32 --retry-wait=1  -q -j 80 -x 16 https://exawestp${b}.blob.core.windows.net/exa-west/test${mc}/data/1G_$i.dat
+echo "RC $?"
+ls -l 1G_$i.dat
+rm -f 1G_$i.*dat
+echo "=== Mid run end ==="
+echo "=== Mid run end ===" 1>&2
+date
+date 1>&2
+time aria2c --max-tries=32 --retry-wait=1  -q -j 80 -x 16 https://exawestp${b}.blob.core.windows.net/exa-west/test${mc}/data/1G_$i.dat
+echo "RC $?"
+ls -l 1G_$i.dat
+rm -f 1G_$i.*dat
+date
+date 1>&2
+
