f4b68c6000b9365de58b8856acd39c6267d609da
Igor Sfiligoi <isfiligoi@ucsd.edu>
2019-10-16 18:09:07 -0700

    Add Azure launch support

---
+++ setup/launch/azure/vmss_cpu_0.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+now=`date +%s`
+mkdir -p /tmp/avmssc0_${now}
+cd /tmp/avmssc0_${now}
+for n in exav7d2uswest2 exav7d2useast1 exav7d2ussc exav7d2usnc exav7d2euwest exav7d2eunorth exav7d2uksouth exav7d2apse exav7d2apjp exav7d2apau exav7d2apko exav7d2canada; do \
+  az vmss scale  --resource-group exa --name ${n}  --new-capacity 0 > ${n}.log &
+  # sleep a bit in between so not to overwhelm the system
+  sleep 1
+done
+echo Logs in /tmp/avmssc0_${now}
+wait
