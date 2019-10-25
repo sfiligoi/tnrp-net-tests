79e298ab863d812ed419a03b837a69291b1d334d
Igor Sfiligoi <isfiligoi@ucsd.edu>
2019-10-25 15:47:47 -0700

    Update Google submit configs

--- setup/launch/gcp/igrp_cpu_1.sh
+++ setup/launch/gcp/igrp_cpu_1.sh
@@ -13,17 +13,17 @@ done
 for i in 4; do
  igs="${igs} ig-us-west1-n2i${i},us-west1 ig-eu-west1-n2i${i},europe-west1"
 done
- 
+
 now=`date +%s`
-mkdir -p /tmp/gig0_${now}
-cd /tmp/gig0_${now}
-echo Logs in /tmp/gig0_${now}
+mkdir -p /tmp/gig1_${now}
+cd /tmp/gig1_${now}
+echo Logs in /tmp/gig1_${now}
 for n in ${igs}; do 
   ig=`echo $n | awk '{split($1,a,","); print a[1]}'`
   zn=`echo $n | awk '{split($1,a,","); print a[2]}'`
 
-  echo "gcloud beta compute instance-groups managed resize ${ig} --region=${zn} --size=0"
-  gcloud beta compute instance-groups managed resize ${ig} --region=${zn} --size=0 > ${ig}.log &
+  echo "gcloud beta compute instance-groups managed resize ${ig} --region=${zn} --size=1"
+  gcloud beta compute instance-groups managed resize ${ig} --region=${zn} --size=1 > ${ig}.log &
   # sleep a bit in between so not to overwhelm the system
   sleep 1
 done
--- setup/launch/gcp/igrp_cpu_n.sh
+++ setup/launch/gcp/igrp_cpu_n.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+if [ $# -ne 1 ]; then
+  echo "ERROR: Usage: vmss_cpu_n.sh <i_per_vmss>|t[emplate]"
+  exit 1
+fi
+
+capacity=$1
+
+if [ "${capacity:0:1}" == "t" ]; then
+  istemplate=1
+  capacity=VAL
+else
+  istemplate=0
+fi
+
 igs="ig-us-west1-n2,us-west1 ig-us-central1-n2,us-central1 ig-us-east1-n2,us-east1 ig-eu-west1-n2,europe-west1 ig-eu-west4-n2,europe-west4 ig-ap-east1-n2,asia-east1 ig-ap-se1-n2,asia-southeast1"
 
 for i in 1 2; do
@@ -13,20 +27,30 @@ done
 for i in 4; do
  igs="${igs} ig-us-west1-n2i${i},us-west1 ig-eu-west1-n2i${i},europe-west1"
 done
- 
-now=`date +%s`
-mkdir -p /tmp/gig0_${now}
-cd /tmp/gig0_${now}
-echo Logs in /tmp/gig0_${now}
+
+if [ ${istemplate} -eq 0 ]; then
+ now=`date +%s`
+ mkdir -p /tmp/gign_${now}
+ cd /tmp/gign_${now}
+ echo Logs in /tmp/gign_${now}
+else
+  echo "INFO: Generating template"
+fi
+
 for n in ${igs}; do 
   ig=`echo $n | awk '{split($1,a,","); print a[1]}'`
   zn=`echo $n | awk '{split($1,a,","); print a[2]}'`
 
-  echo "gcloud beta compute instance-groups managed resize ${ig} --region=${zn} --size=0"
-  gcloud beta compute instance-groups managed resize ${ig} --region=${zn} --size=0 > ${ig}.log &
-  # sleep a bit in between so not to overwhelm the system
-  sleep 1
+  echo "gcloud beta compute instance-groups managed resize ${ig} --region=${zn} --size=${capacity}"
+  if [ ${istemplate} -eq 0 ]; then
+    gcloud beta compute instance-groups managed resize ${ig} --region=${zn} --size=${capacity} > ${ig}.log &
+    # sleep a bit in between so not to overwhelm the system
+    sleep 1
+  fi
 done
-echo "Waiting for completion"
-wait
+
+if [ ${istemplate} -eq 0 ]; then
+  echo "Waiting for completion"
+  wait
+fi
 
