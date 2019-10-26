1bd046cfee2322e3c197ecc2ee1d5bd8d7ed60d2
Igor Sfiligoi <isfiligoi@ucsd.edu>
2019-10-25 17:51:40 -0700

    Add more Azure cpu vmss

--- setup/launch/azure/vmss_cpu_n.sh
+++ setup/launch/azure/vmss_cpu_n.sh
@@ -21,16 +21,16 @@ vvs="exav7d2uswest2 exav7d2useast1 exav7d2ussc exav7d2usnc exav7d2euwest exav7d2
 vvs="${vvs} exav7d1uswest2 exav7d1useast1 exav7d1ussc exav7d1usnc exav7d1euwest exav7d1eunorth exav7d1apse exav7d1apjp exav7d1apau"
 
 # the hugher count ones, interleaved
-for i in 1 2 3; do
-  vvs="${vvs} exav7d2uswest2i${i} exav7d2useast1i${i} exav7d2usnci${i} exav7d2euwesti${i} exav7d2eunorthi${i} exav7d2uksouthi${i} exav7d2apsei${i}"
+for i in 1 2 3 4; do
+  vvs="${vvs} exav7d2uswest2i${i} exav7d2useast1i${i} exav7d2usnci${i} exav7d2euwesti${i} exav7d2eunorthi${i} exav7d2uksouthi${i} exav7d2apsei${i} exav7d1useast1i${i} exav7d1ussci${i}"
 done
 
-for i in 4 5 6 7 8 9; do
-  vvs="${vvs} exav7d2uswest2i${i} exav7d2useast1i${i} exav7d2euwesti${i}"
+for i in 5 6 7 8 9; do
+  vvs="${vvs} exav7d2uswest2i${i} exav7d2useast1i${i} exav7d2euwesti${i} exav7d2eunorthi${i} exav7d2uksouthi${i} exav7d2apsei${i} exav7d1useast1i${i} exav7d1ussci${i}"
 done
 
-for i in a b; do
-  vvs="${vvs} exav7d2useast1i${i} exav7d2euwesti${i}"
+for i in a b c d e f g; do
+  vvs="${vvs} exav7d2uswest2i${i} exav7d2useast1i${i} exav7d2euwesti${i} exav7d1useast1i${i} exav7d1ussci${i}"
 done
 
 if [ ${istemplate} -eq 0 ]; then
