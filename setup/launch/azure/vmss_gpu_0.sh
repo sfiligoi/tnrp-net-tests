ec62b2daf0bf31b399e879d7bfc07afd4d2de873
Igor Sfiligoi <isfiligoi@ucsd.edu>
2019-11-12 01:02:00 -0800

    Update Azure GPU setup

--- setup/launch/azure/vmss_gpu_0.sh
+++ setup/launch/azure/vmss_gpu_0.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 # base one per Region
-vvs="exav7ncuswest2 exav7nc2uswest2 exav7nc3uswest2 exav7nduseast1 exav7nc2useast1 exav7nc3useast1"
+vvs=""
+vvs="${vvs} exav7ncuswest2 exav7nc2uswest2 exav7nc3uswest2"
+vvs="${vvs} exav7nvuseast1 exav7nv3useast1 exav7nduseast1 exav7ncuseast1 exav7nc2useast1 exav7nc3useast1"
 vvs="${vvs} exav7nc2ussc exav7nc3ussc exav7ncusnc exav7nvusnc"
 vvs="${vvs} exav7ndeuwest exav7c2euwest"
 vvs="${vvs} exav7nvapse exav7ndapse exav7ncv3apau"
+
+vvs="${vvs} exav7nvuseast1-i2 exav7nv3useast1-i2 exav7nduseast1-i2 exav7ncuseast1-i2 exav7nc2useast1-i2"
+vvs="${vvs} exav7nvuseast1-i3 exav7nv3useast1-i3"
+
 
 now=`date +%s`
 mkdir -p /tmp/avmssg0_${now}
--- setup/launch/azure/vmss_gpu_n.sh
+++ setup/launch/azure/vmss_gpu_n.sh
@@ -15,10 +15,15 @@ else
 fi
 
 # base one per Region
-vvs="exav7ncuswest2 exav7nc2uswest2 exav7nc3uswest2 exav7nduseast1 exav7nc2useast1 exav7nc3useast1"
+vvs=""
+vvs="${vvs} exav7ncuswest2 exav7nc2uswest2 exav7nc3uswest2"
+vvs="${vvs} exav7nvuseast1 exav7nv3useast1 exav7nduseast1 exav7ncuseast1 exav7nc2useast1 exav7nc3useast1"
 vvs="${vvs} exav7nc2ussc exav7nc3ussc exav7ncusnc exav7nvusnc"
 vvs="${vvs} exav7ndeuwest exav7c2euwest"
 vvs="${vvs} exav7nvapse exav7ndapse exav7ncv3apau"
+
+vvs="${vvs} exav7nvuseast1-i2 exav7nv3useast1-i2 exav7nduseast1-i2 exav7ncuseast1-i2 exav7nc2useast1-i2"
+vvs="${vvs} exav7nvuseast1-i3 exav7nv3useast1-i3"
 
 
 if [ ${istemplate} -eq 0 ]; then
