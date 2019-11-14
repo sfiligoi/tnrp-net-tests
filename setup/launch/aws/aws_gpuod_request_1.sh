79544c49b4dc8b72de0dd27b3fa9a0cffc6f0fa7
Igor Sfiligoi <isfiligoi@ucsd.edu>
2019-11-13 17:14:42 -0800

    Add demo GPU setup for USEast

---
+++ setup/launch/aws/aws_gpuod_request_1.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#regions="us-east-1 us-east-2 eu-west-1 eu-central-1 us-west-2 us-west-1 ap-southeast-1 ap-southeast-2 ap-northeast-1"
+#regions="us-east-1"
+
+tmpl=""
+
+for g in p3 g4 g2 g3 p2; do
+  tmpl="${tmpl} us-east-1_gpuod_${n},us-east-1"
+done
+
+now=`date +%s`
+mkdir -p /tmp/afrg0_${now}
+cd /tmp/afrg0_${now}
+echo Logs in /tmp/afrg0_${now}
+
+for n in ${tmpl}; do
+   r=`echo $n | awk '{split($1,a,","); print a[1]}'`
+   zn=`echo $n | awk '{split($1,a,","); print a[2]}'`
+
+   cat /opt/aws_cmds/templates/${r}_gpuod.json | sed 's/TargetCapacity": 2,/TargetCapacity": 1,/' > ${r}_gpuod.json
+   echo "aws ec2 request-spot-fleet --region ${zn} --spot-fleet-request-config file://$PWD/${r}_gpuod.json"
+   aws ec2 request-spot-fleet --region ${zn} --spot-fleet-request-config file://$PWD/${r}_gpuod.json > ${r}_gpuod.id &
+   sleep 1
+done
+echo "Waiting"
+wait
