4d6279e3b6deca50e452028affb86f5b75ccac9b
Igor Sfiligoi <isfiligoi@ucsd.edu>
2019-11-13 17:29:30 -0800

    Add demo GPU setup for USEast2

--- setup/launch/aws/aws_gpuod_request_1.sh
+++ setup/launch/aws/aws_gpuod_request_1.sh
@@ -6,8 +6,12 @@
 tmpl=""
 
 for g in p3 g4 g2 g3 p2; do
-  tmpl="${tmpl} us-east-1_gpuod_${n},us-east-1"
+  tmpl="${tmpl} us-east-1_gpuod_${g},us-east-1"
 done
+for g in p3 g4 g3 p2; do
+  tmpl="${tmpl} us-east-2_gpuod_${n},us-east-2"
+done
+
 
 now=`date +%s`
 mkdir -p /tmp/afrg0_${now}
@@ -15,12 +19,12 @@ cd /tmp/afrg0_${now}
 echo Logs in /tmp/afrg0_${now}
 
 for n in ${tmpl}; do
-   r=`echo $n | awk '{split($1,a,","); print a[1]}'`
+   t=`echo $n | awk '{split($1,a,","); print a[1]}'`
    zn=`echo $n | awk '{split($1,a,","); print a[2]}'`
 
-   cat /opt/aws_cmds/templates/${r}_gpuod.json | sed 's/TargetCapacity": 2,/TargetCapacity": 1,/' > ${r}_gpuod.json
-   echo "aws ec2 request-spot-fleet --region ${zn} --spot-fleet-request-config file://$PWD/${r}_gpuod.json"
-   aws ec2 request-spot-fleet --region ${zn} --spot-fleet-request-config file://$PWD/${r}_gpuod.json > ${r}_gpuod.id &
+   cat /opt/aws_cmds/templates/${t}.json | sed 's/TargetCapacity": 2,/TargetCapacity": 1,/' > ${t}.json
+   echo "aws ec2 request-spot-fleet --region ${zn} --spot-fleet-request-config file://$PWD/${t}.json"
+   aws ec2 request-spot-fleet --region ${zn} --spot-fleet-request-config file://$PWD/${t}.json > ${t}.id &
    sleep 1
 done
 echo "Waiting"
