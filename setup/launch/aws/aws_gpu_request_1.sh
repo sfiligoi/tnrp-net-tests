bcf6d586a2cfe1a46649b880441bbea6d04f421e
Igor Sfiligoi <isfiligoi@ucsd.edu>
2019-11-13 20:53:36 -0800

    Add eu-central-1

--- setup/launch/aws/aws_gpu_request_1.sh
+++ setup/launch/aws/aws_gpu_request_1.sh
@@ -1,16 +1,40 @@
 #!/bin/bash
 
-regions="us-east-1 us-east-2 eu-west-1 eu-central-1 us-west-2 ap-southeast-1"
+
+tmpl=""
+
+for r in eu-central-1; do
+  tmpl="${tmpl} ${r}_gpuod_g4,${r}"
+done
+
+for r in eu-central-1; do
+  tmpl="${tmpl} ${r}_gpuod_p3,${r}"
+done
+
+for r in eu-central-1; do
+  tmpl="${tmpl} ${r}_gpuod_g2,${r}"
+done
+
+for r in eu-central-1; do
+  tmpl="${tmpl} ${r}_gpuod_p2,${r}"
+done
+
+for r in eu-central-1; do
+  tmpl="${tmpl} ${r}_gpuod_g3,${r}"
+done
 
 now=`date +%s`
-mkdir -p /tmp/afrg1_${now}
-cd /tmp/afrg1_${now}
-echo Logs in /tmp/afrg1_${now}
+mkdir -p /tmp/afrg0_${now}
+cd /tmp/afrg0_${now}
+echo Logs in /tmp/afrg0_${now}
 
-for r in ${regions}; do
-   cat /opt/aws_cmds/templates/${r}_gpus.json | sed 's/"TargetCapacity": 2,/"TargetCapacity": 1,/' > ${r}_gpus.json
-   echo "aws ec2 request-spot-fleet --region ${r} --spot-fleet-request-config file://$PWD/${r}_gpus.json"
-   aws ec2 request-spot-fleet --region ${r} --spot-fleet-request-config file://$PWD/${r}_gpus.json > ${r}_gpus.id &
+for n in ${tmpl}; do
+   t=`echo $n | awk '{split($1,a,","); print a[1]}'`
+   zn=`echo $n | awk '{split($1,a,","); print a[2]}'`
+
+   cat /opt/aws_cmds/templates/${t}.json | sed 's/TargetCapacity": 2,/TargetCapacity": 1,/' > ${t}.json
+   echo "aws ec2 request-spot-fleet --region ${zn} --spot-fleet-request-config file://$PWD/${t}.json"
+   aws ec2 request-spot-fleet --region ${zn} --spot-fleet-request-config file://$PWD/${t}.json > ${t}.id &
    sleep 1
 done
 echo "Waiting"
