80b86c6705e6277619ef67a11c74fde9f015282d
Igor Sfiligoi <isfiligoi@ucsd.edu>
2020-02-01 13:33:36 -0800

    Add PRP Dockerfile

---
+++ cloudburst2_setup/wn/docker/cudalib.sh
@@ -0,0 +1,1 @@
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/lib64:/usr/local/cuda-10.2/targets/x86_64-linux/lib
---
+++ cloudburst2_setup/wn/prp/count_slots.config.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+#
+# Cannot use system info, use fixed values
+#
+
+if [ ! -f "/dev/shm/count_slots.config" ]; then
+  fname=`mktemp -p /dev/shm --suffix=.config cpiXXXX`
+  ngpus=`clinfo -l |grep Device |wc -l`
+
+  # PRP hardcoded
+  ncpus=2
+  allmem=16000
+
+  let leftcpus="${ncpus} - 2 * ${ngpus}"
+
+  # Condor will fail is there is not enough memory per slot, better tune down
+  let cpumem=${allmem}-${ngpus}*10000
+  let maxcpus=${cpumem}/2000
+  if [ ${maxcpus} -lt ${leftcpus} ]; then
+    leftcpus=${maxcpus}
+  fi
+
+  # just protect
+  if [ ${leftcpus} -lt 0 ]; then
+    leftcpus=0
+  fi
+
+  # will count max slots in relation to GPUs
+  # but allow for 0 gpus for testing purposes
+  ncgpus=${ngpus}
+  if [ ${ncgpus} -lt 1 ]; then 
+    ncgpus=1
+  fi
+
+  # max 4 CPU slots per GPU slot
+  let maxcpus=${ncgpus}*4
+  if [ ${leftcpus} -gt ${maxcpus} ]; then
+    leftcpus=${maxcpus}
+  fi
+
+  # cannot afford too many CPU slots, or we will run out of disk space
+  if [ ${leftcpus} -gt 10 ]; then
+    leftcpus=10
+  fi
+
+  echo "NUM_SLOTS_TYPE_1 = ${ngpus}"    >  ${fname}
+  echo "NUM_SLOTS_TYPE_2 = ${leftcpus}" >> ${fname}
+  mv ${fname} /dev/shm/count_slots.config
+  chmod a+r /dev/shm/count_slots.config
+fi
+
+cat /dev/shm/count_slots.config
