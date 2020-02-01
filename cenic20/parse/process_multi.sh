4d0ad8dca09ed41d2ecd0b45923b8f15fba0fba3
Igor Sfiligoi <isfiligoi@ucsd.edu>
2020-02-01 00:52:33 +0000

    Add digested data

--- cenic20/parse/process_multi.sh
+++ cenic20/parse/process_multi.sh
@@ -15,32 +15,20 @@ for i in max.373 max.382 world-s3usc.769 world-s3usc.791 world-gsusc.821 world-g
  done
 done
 
-for j in 658 667; do
- if [ ! -f data_multi/all-regions.multi-max-long.all.${j}.out ]; then
-   echo all-regions.multi-max-long.all.${j}.out
-   ./parse_icecube_long.py ../condor/out/all-regions.multi-max-long.${j}/data.*long* >data_multi/all-regions.multi-max-long.all.${j}.out
+for i in max-long.658 max-long.667 world-long.676 world-long-gsusc.892 world-long-gsusc.914; do
+ t=`echo $i | awk '{split($0,a,"."); print a[1]}'`
+ j=`echo $i | awk '{split($0,a,"."); print a[2]}'`
+ if [ ! -f data_multi/all-regions.multi-${t}.all.${j}.out ]; then
+   echo all-regions.multi-${t}.all.${j}.out
+   ./parse_icecube_long.py ../condor/out/all-regions.multi-${t}.${j}/data.*long* >data_multi/all-regions.multi-${t}.all.${j}.out
  fi
  for c in AWS AZURE GCP; do
-   if [ ! -f data_multi/all-regions.multi-max-long.${c}.${j}.out ]; then
-     echo all-regions.multi-max-long.${c}.${j}.out
-     ./parse_icecube_long.py ../condor/out/all-regions.multi-max-long.${j}/data.${c}*long* >data_multi/all-regions.multi-max-long.${c}.${j}.out
+   if [ ! -f data_multi/all-regions.multi-${t}.${c}.${j}.out ]; then
+     echo all-regions.multi-${t}.${c}.${j}.out
+     ./parse_icecube_long.py ../condor/out/all-regions.multi-${t}.${j}/data.${c}*long* >data_multi/all-regions.multi-${t}.${c}.${j}.out
    fi
  done
 done
-
-for j in 676; do
- if [ ! -f data_multi/all-regions.multi-world-long.all.${j}.out ]; then
-   echo all-regions.multi-world-long.all.${j}.out
-   ./parse_icecube_long.py ../condor/out/all-regions.multi-world-long.${j}/data.*long* >data_multi/all-regions.multi-world-long.all.${j}.out
- fi
- for c in AWS AZURE GCP; do
-   if [ ! -f data_multi/all-regions.multi-world-long.${c}.${j}.out ]; then
-     echo all-regions.multi-world-long.${c}.${j}.out
-     ./parse_icecube_long.py ../condor/out/all-regions.multi-world-long.${j}/data.${c}*long* >data_multi/all-regions.multi-world-long.${c}.${j}.out
-   fi
- done
-done
-
 
 els=`(cd ../condor/out && ls -1 data.*multi.0.out)`
 for f in $els; do
