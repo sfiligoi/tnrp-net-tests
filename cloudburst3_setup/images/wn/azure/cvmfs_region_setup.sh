8a48dc8d49cc7324e907a5c9624afdd46be4fc0d
Igor Sfiligoi <isfiligoi@ucsd.edu>
2020-09-30 13:18:55 -0700

    Update regions

--- cloudburst3_setup/images/wn/azure/cvmfs_region_setup.sh
+++ cloudburst3_setup/images/wn/azure/cvmfs_region_setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#
+# Ask the metadata system where I am
+#
+
 fname=`mktemp -p /dev/shm/ --suffix=.config crcXXXX`
 myregion=`curl -s -H Metadata:true "http://169.254.169.254/metadata/instance/compute/location?api-version=2017-08-01&format=text"`
 if [ ! -f /etc/condor/regions/${myregion}_cidr.config ]; then
@@ -20,10 +24,19 @@ fi
 if [ ! -f /etc/condor/regions/${myregion}_cidr.config ]; then
   myregion=invalid
 fi
+
+#
+# Save for future use by e.g. HTCondor
+#
+
 echo "${myregion}" > ${fname}
 mv ${fname} /dev/shm/region.config
 chmod a+r /dev/shm/region.config
 
+
+#
+# Find where is the closest CVMFS and related services
+#
 
 for ip in `hostname -i`; do
   cidr=`/etc/condor/scripts/parse_cidr.sh /etc/condor/regions/${myregion}_cidr.config "$ip"`
@@ -32,13 +45,21 @@ for ip in `hostname -i`; do
   fi
 done
 
+#
+# Save cvmfs and HTCondor config files
+#
+
 if [ "x$cidr" != "x" ] ; then
   chead=`echo "$cidr" |awk '{print $2}'`
   squid=`echo "$cidr" |awk '{print $3}'`
+  peering=`echo "$cidr" |awk '{print $4}'`
 
   if [ -f /etc/condor/regions/${myregion}_local.config ]; then
     cp /etc/condor/regions/${myregion}_local.config /dev/shm/my_local.config
     echo "CLOUD_HEAD = ${chead}" >> /dev/shm/my_local.config
+    echo "${peering}_Accessible = true" >> /dev/shm/my_local.config
+    echo "STARTD_EXPRS = \$(STARTD_EXPRS) ${peering}_Accessible" >> /dev/shm/my_local.config
+
   else
     cp /etc/condor/regions/invalid_local.config /dev/shm/my_local.config
   fi
@@ -47,6 +68,8 @@ else
   # just put some invalid values, i.e. localhost
   chead=127.0.0.1
   squid=127.0.0.1 
+  peering=none
+
   cp /etc/condor/regions/invalid_local.config /dev/shm/my_local.config
 fi
 
