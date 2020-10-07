a175a244012b4a24970842db07d4be4c98954279
Igor Sfiligoi <isfiligoi@ucsd.edu>
2020-10-07 09:37:49 -0700

    Add race condition check

--- cloudburst3_setup/images/wn/get_local.config.sh
+++ cloudburst3_setup/images/wn/get_local.config.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 if [ ! -f "/dev/shm/my_local.config" ]; then
+  # Deal with race conditions
+  sleep 5
+fi
+if [ ! -f "/dev/shm/my_local.config" ]; then
+  # Deal with race conditions
+  sleep 30
+fi
+
+if [ ! -f "/dev/shm/my_local.config" ]; then
   # should never get in here, but just in case
   cp /etc/condor/regions/invalid_local.config /dev/shm/my_local.config
   chmod a+r /dev/shm/my_local.config
