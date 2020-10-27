41b07803e5bc6e897db473b38d5788f0bfe4f305
Igor Sfiligoi <isfiligoi@ucsd.edu>
2020-10-27 08:35:57 -0700

    Switch to v2 and group zones in central igs

--- cloudburst3_setup/images/wn-groups/gcp/gcp_create_instance_groups.sh
+++ cloudburst3_setup/images/wn-groups/gcp/gcp_create_instance_groups.sh
@@ -2,36 +2,39 @@
 
 # US east
 #z1
-for z in a b f; do
-    gcloud compute --project=exa-demo instance-groups managed create ig-exa3-us-central-1-${z}-t4 \
-                   --base-instance-name=ig-exa3-us-central-1-${z}-t4 --template=it-exa3-us-central-1-t4-v1 --size=0 \
-                   --zones=us-central1-${z} --instance-redistribution-type=NONE
-done
+gcloud compute --project=exa-demo instance-groups managed create ig-exa3-us-central-1-t4 \
+               --base-instance-name=ig-exa3-us-central-1-t4 --template=it-exa3-us-central-1-t4-v2 --size=0 \
+               --zones=us-central1-a,us-central1-b,us-central1-f --instance-redistribution-type=NONE
 
-for z in a b c f; do
-    gcloud compute --project=exa-demo instance-groups managed create ig-exa3-us-central-1-${z}-v100 \
-                   --base-instance-name=ig-exa3-us-central-1-${z}-v100 --template=it-exa3-us-central-1-v100-v1 --size=0 \
-                   --zones=us-central1-${z} --instance-redistribution-type=NONE
-done
+gcloud compute --project=exa-demo instance-groups managed create ig-exa3-us-central-1-v100 \
+               --base-instance-name=ig-exa3-us-central-1-v100 --template=it-exa3-us-central-1-v100-v2 --size=0 \
+               --zones=us-central1-a,us-central1-b,us-central1-c,us-central1-f --instance-redistribution-type=NONE
 
 #z2
-for z in a b f; do
-    gcloud compute --project=exa-demo instance-groups managed create ig-exa3b-us-central-1-${z}-t4 \
-                   --base-instance-name=ig-exa3b-us-central-1-${z}-t4 --template=it-exa3b-us-central-1-t4-v1 --size=0 \
-                   --zones=us-central1-${z} --instance-redistribution-type=NONE
-done
+gcloud compute --project=exa-demo instance-groups managed create ig-exa3b-us-central-1-t4 \
+               --base-instance-name=ig-exa3b-us-central-1-t4 --template=it-exa3b-us-central-1-t4-v2 --size=0 \
+               --zones=us-central1-a,us-central1-b,us-central1-f --instance-redistribution-type=NONE
 
-for z in a b c f; do
-    gcloud compute --project=exa-demo instance-groups managed create ig-exa3b-us-central-1-${z}-v100 \
-                   --base-instance-name=ig-exa3-usb-central-1-${z}-v100 --template=it-exa3b-us-central-1-v100-v1 --size=0 \
-                   --zones=us-central1-${z} --instance-redistribution-type=NONE
-done
+gcloud compute --project=exa-demo instance-groups managed create ig-exa3b-us-central-1-v100 \
+               --base-instance-name=ig-exa3b-us-central-1-v100 --template=it-exa3b-us-central-1-v100-v2 --size=0 \
+               --zones=us-central1-a,us-central1-b,us-central1-c,us-central1-f --instance-redistribution-type=NONE
+
+
+#z3
+
+gcloud compute --project=exa-demo instance-groups managed create ig-exa3c-us-central-1-t4 \
+               --base-instance-name=ig-exa3c-us-central-1-t4 --template=it-exa3c-us-central-1-t4-v2 --size=0 \
+               --zones=us-central1-a,us-central1-b,us-central1-f --instance-redistribution-type=NONE
+
+gcloud compute --project=exa-demo instance-groups managed create ig-exa3c-us-central-1-v100 \
+               --base-instance-name=ig-exa3c-us-central-1-v100 --template=it-exa3c-us-central-1-v100-v2 --size=0 \
+               --zones=us-central1-a,us-central1-b,us-central1-c,us-central1-f --instance-redistribution-type=NONE
 
 
 # US West
 for z in a b; do
     gcloud compute --project=exa-demo instance-groups managed create ig-exa3-us-west1-${z}-t4 \
-                   --base-instance-name=ig-exa3-us-west1-${z}-t4 --template=it-exa3-us-west-1-t4-v1 --size=0 \
+                   --base-instance-name=ig-exa3-us-west1-${z}-t4 --template=it-exa3-us-west-1-t4-v2 --size=0 \
                    --zones=us-west1-${z} --instance-redistribution-type=NONE
 done
 
@@ -55,7 +58,7 @@ done
 
 for z in a b; do
     gcloud compute --project=exa-demo instance-groups managed create ig-exa3-us-west1-${z}-v100 \
-                   --base-instance-name=ig-exa3-us-west1-${z}-v100 --template=it-exa3-us-west-1-v100-v1 --size=0 \
+                   --base-instance-name=ig-exa3-us-west1-${z}-v100 --template=it-exa3-us-west-1-v100-v2 --size=0 \
                    --zones=us-west1-${z} --instance-redistribution-type=NONE
 done
 
--- cloudburst3_setup/images/wn-groups/gcp/gcp_create_instance_templates.sh
+++ cloudburst3_setup/images/wn-groups/gcp/gcp_create_instance_templates.sh
@@ -1,16 +1,16 @@
 #!/bin/bash -x
 
-IMAGE='--image-project=exa-demo --image=wn-er-us-v1 --boot-disk-size=30GB --boot-disk-type=pd-standard --machine-type=n1-standard-4 --no-restart-on-failure --maintenance-policy=TERMINATE --preemptible --network-tier=STANDARD'
+IMAGE='--image-project=exa-demo --image=wn-er-us-v2 --boot-disk-size=30GB --boot-disk-type=pd-standard --machine-type=n1-standard-4 --no-restart-on-failure --maintenance-policy=TERMINATE --preemptible --network-tier=STANDARD'
 
 SSHKEY=--metadata=ssh-keys=centos:ssh-rsa\ AAAAB3NzaC1yc2EAAAADAQABAAABAQDIi6sHuPqcx4J0pLiXSnLGRSlnILReeaff9gGCm0zKiqHVeZ6RPo2gbYKDBzVgDJcqOftjp/Mcg30YIDX1ei42W2WRHx/ewuK\+09qejjAuo389ySByS6mI/emu2HhdvYMb07nhycdyK45xXYM6wla6hfQIrNccAKfPCWGdlc/FNxW5ffBFrm7AM9yMCXc95XkuRBto1oaySTYEr3FGYw2U8FfRmmnL/U7xI7zKR4OeWRn8KlaQyABybQ8h5a6T6ISjxmUoSL1MIWPlfNuxZ5djEoCIrtuhPeXW5dxlPrJCdSiBHQcAhsqQD1oyCIz60QCj9xJOjdx4ENXqlkFLPBw9\ centos
 
 #US West
-gcloud compute --project=exa-demo instance-templates create it-exa3-us-west-1-t4-v1 $IMAGE "$SSHKEY" \
+gcloud compute --project=exa-demo instance-templates create it-exa3-us-west-1-t4-v2 $IMAGE "$SSHKEY" \
     --min-cpu-platform="Intel Broadwell" \
     --region=us-west1 --subnet=projects/exa-demo/regions/us-west1/subnetworks/exa3-us-west1 \
     --accelerator=type=nvidia-tesla-t4,count=1 
 
-gcloud compute --project=exa-demo instance-templates create it-exa3-us-west-1-v100-v1 $IMAGE "$SSHKEY" \
+gcloud compute --project=exa-demo instance-templates create it-exa3-us-west-1-v100-v2 $IMAGE "$SSHKEY" \
     --min-cpu-platform="Intel Broadwell" \
     --region=us-west1 --subnet=projects/exa-demo/regions/us-west1/subnetworks/exa3-us-west1 \
     --accelerator=type=nvidia-tesla-v100,count=1
@@ -20,35 +20,35 @@ gcloud compute --project=exa-demo instance-templates create it-exa3-us-west-1-v1
 
 
 # z1
-gcloud compute --project=exa-demo instance-templates create it-exa3-us-central-1-t4-v1 $IMAGE "$SSHKEY" \
+gcloud compute --project=exa-demo instance-templates create it-exa3-us-central-1-t4-v2 $IMAGE "$SSHKEY" \
     --min-cpu-platform="Intel Broadwell" \
     --region=us-central1 --subnet=projects/exa-demo/regions/us-central1/subnetworks/exa3-us-central-1 \
     --accelerator=type=nvidia-tesla-t4,count=1
 
-gcloud compute --project=exa-demo instance-templates create it-exa3-us-central-1-v100-v1 $IMAGE "$SSHKEY" \
+gcloud compute --project=exa-demo instance-templates create it-exa3-us-central-1-v100-v2 $IMAGE "$SSHKEY" \
     --min-cpu-platform="Intel Broadwell" \
     --region=us-central1 --subnet=projects/exa-demo/regions/us-central1/subnetworks/exa3-us-central-1 \
     --accelerator=type=nvidia-tesla-v100,count=1
 
 
 # z2
-gcloud compute --project=exa-demo instance-templates create it-exa3b-us-central-1-t4-v1 $IMAGE "$SSHKEY" \
+gcloud compute --project=exa-demo instance-templates create it-exa3b-us-central-1-t4-v2 $IMAGE "$SSHKEY" \
     --min-cpu-platform="Intel Broadwell" \
     --region=us-central1 --subnet=projects/exa-demo/regions/us-central1/subnetworks/exa3b-us-central-1 \
     --accelerator=type=nvidia-tesla-t4,count=1
 
-gcloud compute --project=exa-demo instance-templates create it-exa3b-us-central-1-v100-v1 $IMAGE "$SSHKEY" \
+gcloud compute --project=exa-demo instance-templates create it-exa3b-us-central-1-v100-v2 $IMAGE "$SSHKEY" \
     --min-cpu-platform="Intel Broadwell" \
     --region=us-central1 --subnet=projects/exa-demo/regions/us-central1/subnetworks/exa3b-us-central-1 \
     --accelerator=type=nvidia-tesla-v100,count=1
 
 # z3
-gcloud compute --project=exa-demo instance-templates create it-exa3c-us-central-1-t4-v1 $IMAGE "$SSHKEY" \
+gcloud compute --project=exa-demo instance-templates create it-exa3c-us-central-1-t4-v2 $IMAGE "$SSHKEY" \
     --min-cpu-platform="Intel Broadwell" \
     --region=us-central1 --subnet=projects/exa-demo/regions/us-central1/subnetworks/exa3c-us-central-1 \
     --accelerator=type=nvidia-tesla-t4,count=1
 
-gcloud compute --project=exa-demo instance-templates create it-exa3c-us-central-1-v100-v1 $IMAGE "$SSHKEY" \
+gcloud compute --project=exa-demo instance-templates create it-exa3c-us-central-1-v100-v2 $IMAGE "$SSHKEY" \
     --min-cpu-platform="Intel Broadwell" \
     --region=us-central1 --subnet=projects/exa-demo/regions/us-central1/subnetworks/exa3c-us-central-1 \
     --accelerator=type=nvidia-tesla-v100,count=1
