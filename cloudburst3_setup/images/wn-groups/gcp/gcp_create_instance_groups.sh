96e8e6fd2f717811690e5b3c1838d7f2fcdb3906
Igor Sfiligoi <isfiligoi@ucsd.edu>
2020-10-27 08:05:12 -0700

    Add more zones

--- cloudburst3_setup/images/wn-groups/gcp/gcp_create_instance_groups.sh
+++ cloudburst3_setup/images/wn-groups/gcp/gcp_create_instance_groups.sh
@@ -1,12 +1,34 @@
 #!/bin/bash -x
 
-#for z in a b f; do
-#    gcloud compute --project=exa-demo instance-groups managed create ig-exa3-us-central1-${z}-t4 \
-#                   --base-instance-name=ig-exa3-us-central1-${z}-t4 --template=it-exa3-us-t4-v1 --size=0 \
-#                   --zones=us-central1-${z} --instance-redistribution-type=NONE
-#done
+# US east
+#z1
+for z in a b f; do
+    gcloud compute --project=exa-demo instance-groups managed create ig-exa3-us-central-1-${z}-t4 \
+                   --base-instance-name=ig-exa3-us-central-1-${z}-t4 --template=it-exa3-us-central-1-t4-v1 --size=0 \
+                   --zones=us-central1-${z} --instance-redistribution-type=NONE
+done
+
+for z in a b c f; do
+    gcloud compute --project=exa-demo instance-groups managed create ig-exa3-us-central-1-${z}-v100 \
+                   --base-instance-name=ig-exa3-us-central-1-${z}-v100 --template=it-exa3-us-central-1-v100-v1 --size=0 \
+                   --zones=us-central1-${z} --instance-redistribution-type=NONE
+done
+
+#z2
+for z in a b f; do
+    gcloud compute --project=exa-demo instance-groups managed create ig-exa3b-us-central-1-${z}-t4 \
+                   --base-instance-name=ig-exa3b-us-central-1-${z}-t4 --template=it-exa3b-us-central-1-t4-v1 --size=0 \
+                   --zones=us-central1-${z} --instance-redistribution-type=NONE
+done
+
+for z in a b c f; do
+    gcloud compute --project=exa-demo instance-groups managed create ig-exa3b-us-central-1-${z}-v100 \
+                   --base-instance-name=ig-exa3-usb-central-1-${z}-v100 --template=it-exa3b-us-central-1-v100-v1 --size=0 \
+                   --zones=us-central1-${z} --instance-redistribution-type=NONE
+done
 
 
+# US West
 for z in a b; do
     gcloud compute --project=exa-demo instance-groups managed create ig-exa3-us-west1-${z}-t4 \
                    --base-instance-name=ig-exa3-us-west1-${z}-t4 --template=it-exa3-us-west-1-t4-v1 --size=0 \
@@ -23,6 +45,7 @@ done
 #
 #
 #
+
 
 #for z in a b c f; do
 #    gcloud beta compute --project=exa-demo instance-groups managed create ig-us-central1-${z}-v100-v8 \
