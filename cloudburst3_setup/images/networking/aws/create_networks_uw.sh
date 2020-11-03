a793d03e119c113ca6ed84719597937b16074eb4
Igor Sfiligoi <isfiligoi@ucsd.edu>
2020-11-03 09:55:49 -0800

    Update AWS networking

--- cloudburst3_setup/images/networking/aws/create_networks_uw.sh
+++ cloudburst3_setup/images/networking/aws/create_networks_uw.sh
@@ -3,7 +3,7 @@
 
 #vpc-exa3-east-1
 aws --region us-east-1 ec2 create-vpc --cidr-block 10.250.0.0/19  "--tag-specifications=ResourceType=vpc,Tags=[{Key=Name,Value=vpc-exa3-east-1},{Key=ClouddConnect,Value=True}]"
-VPC=vpc-0b3474d2377951990
+VPC=vpc-0fc9ab2b10bb9eba6
 IPel=0
 for z in a b c d e f ; do 
   aws --region us-east-1 ec2 create-subnet --vpc-id=${VPC} --cidr-block=10.250.${IPel}.0/22 --availability-zone=us-east-1${z} "--tag-specifications=ResourceType=subnet,Tags=[{Key=Name,Value=sn-exa3-east-1${z}},{Key=ClouddConnect,Value=True}]"
@@ -12,7 +12,7 @@ done
 
 #vpc-exa3b-east-1
 aws --region us-east-1 ec2 create-vpc --cidr-block 10.250.32.0/19  "--tag-specifications=ResourceType=vpc,Tags=[{Key=Name,Value=vpc-exa3b-east-1},{Key=ClouddConnect,Value=True}]"
-VPC=vpc-0698ae5de81f47d2e
+VPC=vpc-0aef4762503d2348c
 IPel=32
 for z in a b c d e f ; do
   aws --region us-east-1 ec2 create-subnet --vpc-id=${VPC} --cidr-block=10.250.${IPel}.0/22 --availability-zone=us-east-1${z} "--tag-specifications=ResourceType=subnet,Tags=[{Key=Name,Value=sn-exa3b-east-1${z}},{Key=ClouddConnect,Value=True}]"
@@ -30,11 +30,37 @@ done
 
 #vpc-exa3b-east-2
 aws --region us-east-2 ec2 create-vpc --cidr-block 10.250.80.0/20  "--tag-specifications=ResourceType=vpc,Tags=[{Key=Name,Value=vpc-exa3b-east-2},{Key=ClouddConnect,Value=True}]"
-VPC=???
+VPC=vpc-049981127d8ae6336
 IPel=80
 for z in a b c ; do
   aws --region us-east-2 ec2 create-subnet --vpc-id=${VPC} --cidr-block=10.250.${IPel}.0/22 --availability-zone=us-east-2${z} "--tag-specifications=ResourceType=subnet,Tags=[{Key=Name,Value=sn-exa3b-east-2${z}},{Key=ClouddConnect,Value=True}]"
   let IPel=IPel+4
 done
 
+#vpc-exa3c-east-2
+aws --region us-east-2 ec2 create-vpc --cidr-block 10.250.112.0/20  "--tag-specifications=ResourceType=vpc,Tags=[{Key=Name,Value=vpc-exa3c-east-2},{Key=ClouddConnect,Value=True}]"
+VPC=vpc-0d9a008073d6c662d
+IPel=112
+for z in a b c ; do
+  aws --region us-east-2 ec2 create-subnet --vpc-id=${VPC} --cidr-block=10.250.${IPel}.0/22 --availability-zone=us-east-2${z} "--tag-specifications=ResourceType=subnet,Tags=[{Key=Name,Value=sn-exa3c-east-2${z}},{Key=ClouddConnect,Value=True}]"
+  let IPel=IPel+4
+done
+
+
+
+#vpc-ucsd3-west-1
+aws --region us-west-1 ec2 create-vpc --cidr-block 10.250.96.0/20  "--tag-specifications=ResourceType=vpc,Tags=[{Key=Name,Value=vpc-exa3-west-1},{Key=ClouddConnect,Value=True}]"
+VPC=vpc-0f78bed8b0c47c1c1
+IPel=96
+for z in b c ; do
+  aws --region us-west-1 ec2 create-subnet --vpc-id=${VPC} --cidr-block=10.250.${IPel}.0/21 --availability-zone=us-west-1${z} "--tag-specifications=ResourceType=subnet,Tags=[{Key=Name,Value=sn-exa3-west-1${z}},{Key=ClouddConnect,Value=True}]"
+  let IPel=IPel+8
+done
+
+
+# manually create 
+#   internet gateways
+#   virtual private gateways
+#   peering with defalult
+#   routes
 
