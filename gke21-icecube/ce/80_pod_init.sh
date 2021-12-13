0a1122b702363de1a0f9402435b48a154d4df41e
Igor Sfiligoi <isfiligoi@sdsc.edu>
2021-12-13 22:22:36 +0000

    Add osg-ce yaml

---
+++ gke21-icecube/ce/80_pod_init.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Point to the collector
+# The value is porpagated in the environment by the osg_collector_prp service 
+echo "CONDOR_HOST = ${OSG_COLLECTOR_SERVICE_HOST}" > /etc/condor/config.d/50_condor_host.config
+
+EXTIP=104.198.217.177
+echo "TCP_FORWARDING_HOST = ${EXTIP}" > /etc/condor/config.d/90_nat.config
+echo 'HOST_ALIAS = icecube-cloud-t4-ce.t2.ucsd.edu' >> /etc/condor/config.d/90_nat.config
+
+echo "TCP_FORWARDING_HOST = ${EXTIP}" > /etc/condor-ce/config.d/90_nat.config
+echo 'HOST_ALIAS = icecube-cloud-t4-ce.t2.ucsd.edu' >> /etc/condor-ce/config.d/90_nat.config
+
+
+# If this is the first use of the persistent disk areas, create the necessary dirs
+# This dirs are normally created by the RPM in the base image
+
+if [ -n "/var/lib/condor/spool" ]; then
+  mkdir /var/lib/condor/spool   && chown condor:condor /var/lib/condor/spool
+  mkdir /var/lib/condor/execute && chown condor:condor /var/lib/condor/execute
+fi
+chown condor:condor /var/lib/condor
+
+if [ -n "/var/lib/condor-ce/spool" ]; then
+  mkdir /var/lib/condor-ce/spool   && chown condor:condor /var/lib/condor-ce/spool
+  mkdir /var/lib/condor-ce/execute && chown condor:condor /var/lib/condor-ce/execute
+fi
+chown condor:condor /var/lib/condor-ce
+
+if [ -n "/home/condor" ]; then
+  mkdir /home/condor && chown condor:condor /home/condor
+fi
+
+# List of all supported users
+groupadd -g 3002 icecube && useradd -u 3002 -g 3002 -s /bin/bash icecube
+
+
+cat > /etc/condor/namespace_map << EOF
+# user namespace
+* icecube default
+EOF
+
+
