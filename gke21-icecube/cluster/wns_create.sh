2ab14635325914d323de7f62c2b4317d892fbb77
Igor Sfiligoi <isfiligoi@sdsc.edu>
2022-02-10 18:52:03 +0000

    Add 2x and 1x MIG instance groups

--- gke21-icecube/cluster/wns_create.sh
+++ gke21-icecube/cluster/wns_create.sh
@@ -1,2 +1,8 @@
+# 7x MIG
 gcloud beta container node-pools create pa100m7 --cluster=icecube --zone us-central1-c --node-taints=osglimit=gpu:PreferNoSchedule --node-labels=osgclass=gpu,gputype=a100 --accelerator type=nvidia-tesla-a100,count=1,gpu-partition-size=1g.5gb --machine-type=a2-highgpu-1g --num-nodes=1 --spot
+# 3x MIG
 gcloud beta container node-pools create pa100m3 --cluster=icecube --zone us-central1-c --node-taints=osglimit=gpu:PreferNoSchedule --node-labels=osgclass=gpu,gputype=a100 --accelerator type=nvidia-tesla-a100,count=1,gpu-partition-size=2g.10gb --machine-type=a2-highgpu-1g --num-nodes=1 --spot
+# 2x MIG
+gcloud beta container node-pools create pa100m2 --cluster=icecube --zone us-central1-c --node-taints=osglimit=gpu:PreferNoSchedule --node-labels=osgclass=gpu,gputype=a100 --accelerator type=nvidia-tesla-a100,count=1,gpu-partition-size=3g.20gb --machine-type=a2-highgpu-1g --num-nodes=1 --spot
+# whole A100 (1x MIG)
+gcloud beta container node-pools create pa100m1 --cluster=icecube --zone us-central1-c --node-taints=osglimit=gpu:PreferNoSchedule --node-labels=osgclass=gpu,gputype=a100 --accelerator type=nvidia-tesla-a100,count=1,gpu-partition-size=7g.40gb --machine-type=a2-highgpu-1g --num-nodes=1 --spot
