7a7af9a845d5ffa5ecf03a2d6893102a19a9e173
Igor Sfiligoi <isfiligoi@sdsc.edu>
2022-08-04 17:04:05 +0000

    Add gke22-icecube-arm/scripts

---
+++ gke22-icecube-arm/scripts/x86/run_HE_5.sh
@@ -0,0 +1,38 @@
+echo "=================================="
+echo " Now: `date`"
+echo "=================================="
+time /cvmfs/icecube.opensciencegrid.org/py3-v4.2.0/RHEL_7_x86_64/metaprojects/icetray/v1.3.3/env-shell.sh /usr/bin/time /cvmfs/icecube.opensciencegrid.org/py3-v4.2.0/RHEL_7_x86_64/metaprojects/icetray/v1.3.3/simprod-scripts/resources/scripts/corsika5comp.py --corsikaVersion 76900g --CVMFS /cvmfs/icecube.opensciencegrid.org/py3-v4.2.0 --gcdfile /cvmfs/icecube.opensciencegrid.org/data/GCD/GeoCalibDetectorStatus_2020.Run134142.Pass2_V0.i3.gz --UseGSLRNG --UsePipe --compress --CutoffType EnergyPerParticle --eprimarymin=1e6 --eprimarymax=1e9 --pnorm 10,5,3,2,1 --pgam 2,2,2,2,2 --nshowers 10000 --outputfile corsika_HE.i3.zst
+echo "=================================="
+ls -l
+rm -fr CORSIKA000000.* output* dcors0 corsik*.zst summary.json icecube_icesoft
+echo "=================================="
+echo " Now: `date`"
+echo "=================================="
+time /cvmfs/icecube.opensciencegrid.org/py3-v4.2.0/RHEL_7_x86_64/metaprojects/icetray/v1.3.3/env-shell.sh /usr/bin/time /cvmfs/icecube.opensciencegrid.org/py3-v4.2.0/RHEL_7_x86_64/metaprojects/icetray/v1.3.3/simprod-scripts/resources/scripts/corsika5comp.py --corsikaVersion 76900g --CVMFS /cvmfs/icecube.opensciencegrid.org/py3-v4.2.0 --gcdfile /cvmfs/icecube.opensciencegrid.org/data/GCD/GeoCalibDetectorStatus_2020.Run134142.Pass2_V0.i3.gz --UseGSLRNG --UsePipe --compress --CutoffType EnergyPerParticle --eprimarymin=1e6 --eprimarymax=1e9 --pnorm 10,5,3,2,1 --pgam 2,2,2,2,2 --nshowers 10000 --outputfile corsika_HE.i3.zst
+echo "=================================="
+ls -l
+rm -fr CORSIKA000000.* output* dcors0 corsik*.zst summary.json icecube_icesoft
+echo "=================================="
+echo " Now: `date`"
+echo "=================================="
+time /cvmfs/icecube.opensciencegrid.org/py3-v4.2.0/RHEL_7_x86_64/metaprojects/icetray/v1.3.3/env-shell.sh /usr/bin/time /cvmfs/icecube.opensciencegrid.org/py3-v4.2.0/RHEL_7_x86_64/metaprojects/icetray/v1.3.3/simprod-scripts/resources/scripts/corsika5comp.py --corsikaVersion 76900g --CVMFS /cvmfs/icecube.opensciencegrid.org/py3-v4.2.0 --gcdfile /cvmfs/icecube.opensciencegrid.org/data/GCD/GeoCalibDetectorStatus_2020.Run134142.Pass2_V0.i3.gz --UseGSLRNG --UsePipe --compress --CutoffType EnergyPerParticle --eprimarymin=1e6 --eprimarymax=1e9 --pnorm 10,5,3,2,1 --pgam 2,2,2,2,2 --nshowers 10000 --outputfile corsika_HE.i3.zst
+echo "=================================="
+ls -l
+rm -fr CORSIKA000000.* output* dcors0 corsik*.zst summary.json icecube_icesoft
+echo "=================================="
+echo " Now: `date`"
+echo "=================================="
+time /cvmfs/icecube.opensciencegrid.org/py3-v4.2.0/RHEL_7_x86_64/metaprojects/icetray/v1.3.3/env-shell.sh /usr/bin/time /cvmfs/icecube.opensciencegrid.org/py3-v4.2.0/RHEL_7_x86_64/metaprojects/icetray/v1.3.3/simprod-scripts/resources/scripts/corsika5comp.py --corsikaVersion 76900g --CVMFS /cvmfs/icecube.opensciencegrid.org/py3-v4.2.0 --gcdfile /cvmfs/icecube.opensciencegrid.org/data/GCD/GeoCalibDetectorStatus_2020.Run134142.Pass2_V0.i3.gz --UseGSLRNG --UsePipe --compress --CutoffType EnergyPerParticle --eprimarymin=1e6 --eprimarymax=1e9 --pnorm 10,5,3,2,1 --pgam 2,2,2,2,2 --nshowers 10000 --outputfile corsika_HE.i3.zst
+echo "=================================="
+ls -l
+rm -fr CORSIKA000000.* output* dcors0 corsik*.zst summary.json icecube_icesoft
+echo "=================================="
+echo " Now: `date`"
+echo "=================================="
+time /cvmfs/icecube.opensciencegrid.org/py3-v4.2.0/RHEL_7_x86_64/metaprojects/icetray/v1.3.3/env-shell.sh /usr/bin/time /cvmfs/icecube.opensciencegrid.org/py3-v4.2.0/RHEL_7_x86_64/metaprojects/icetray/v1.3.3/simprod-scripts/resources/scripts/corsika5comp.py --corsikaVersion 76900g --CVMFS /cvmfs/icecube.opensciencegrid.org/py3-v4.2.0 --gcdfile /cvmfs/icecube.opensciencegrid.org/data/GCD/GeoCalibDetectorStatus_2020.Run134142.Pass2_V0.i3.gz --UseGSLRNG --UsePipe --compress --CutoffType EnergyPerParticle --eprimarymin=1e6 --eprimarymax=1e9 --pnorm 10,5,3,2,1 --pgam 2,2,2,2,2 --nshowers 10000 --outputfile corsika_HE.i3.zst
+echo "=================================="
+ls -l
+rm -fr CORSIKA000000.* output* dcors0 corsik*.zst summary.json icecube_icesoft
+echo "=================================="
+echo " Now: `date`"
+echo "=================================="
---
+++ gke22-icecube-arm/scripts/x86/run_ME_5.sh
@@ -0,0 +1,38 @@
+echo "=================================="
+echo " Now: `date`"
+echo "=================================="
+/cvmfs/icecube.opensciencegrid.org/py3-v4.2.0/RHEL_7_x86_64/metaprojects/icetray/v1.3.3/env-shell.sh /usr/bin/time /cvmfs/icecube.opensciencegrid.org/py3-v4.2.0/RHEL_7_x86_64/metaprojects/icetray/v1.3.3/simprod-scripts/resources/scripts/corsika5comp.py --corsikaVersion 76900g --CVMFS /cvmfs/icecube.opensciencegrid.org/py3-v4.2.0 --gcdfile /cvmfs/icecube.opensciencegrid.org/data/GCD/GeoCalibDetectorStatus_2020.Run134142.Pass2_V0.i3.gz --UseGSLRNG --UsePipe --compress --CutoffType EnergyPerParticle --eprimarymin=3e4 --eprimarymax=1e6 --pnorm 5,2.25,1.1,1.2,1 --pgam 2.65,2.6,2.6,2.6,2.6 --nshowers 120000 --outputfile corsika_ME.i3.zst
+echo "=================================="
+ls -l
+rm -fr CORSIKA000000.* output* dcors0 corsik*.zst summary.json icecube_icesoft
+echo "=================================="
+echo " Now: `date`"
+echo "=================================="
+/cvmfs/icecube.opensciencegrid.org/py3-v4.2.0/RHEL_7_x86_64/metaprojects/icetray/v1.3.3/env-shell.sh /usr/bin/time /cvmfs/icecube.opensciencegrid.org/py3-v4.2.0/RHEL_7_x86_64/metaprojects/icetray/v1.3.3/simprod-scripts/resources/scripts/corsika5comp.py --corsikaVersion 76900g --CVMFS /cvmfs/icecube.opensciencegrid.org/py3-v4.2.0 --gcdfile /cvmfs/icecube.opensciencegrid.org/data/GCD/GeoCalibDetectorStatus_2020.Run134142.Pass2_V0.i3.gz --UseGSLRNG --UsePipe --compress --CutoffType EnergyPerParticle --eprimarymin=3e4 --eprimarymax=1e6 --pnorm 5,2.25,1.1,1.2,1 --pgam 2.65,2.6,2.6,2.6,2.6 --nshowers 120000 --outputfile corsika_ME.i3.zst
+echo "=================================="
+ls -l
+rm -fr CORSIKA000000.* output* dcors0 corsik*.zst summary.json icecube_icesoft
+echo "=================================="
+echo " Now: `date`"
+echo "=================================="
+/cvmfs/icecube.opensciencegrid.org/py3-v4.2.0/RHEL_7_x86_64/metaprojects/icetray/v1.3.3/env-shell.sh /usr/bin/time /cvmfs/icecube.opensciencegrid.org/py3-v4.2.0/RHEL_7_x86_64/metaprojects/icetray/v1.3.3/simprod-scripts/resources/scripts/corsika5comp.py --corsikaVersion 76900g --CVMFS /cvmfs/icecube.opensciencegrid.org/py3-v4.2.0 --gcdfile /cvmfs/icecube.opensciencegrid.org/data/GCD/GeoCalibDetectorStatus_2020.Run134142.Pass2_V0.i3.gz --UseGSLRNG --UsePipe --compress --CutoffType EnergyPerParticle --eprimarymin=3e4 --eprimarymax=1e6 --pnorm 5,2.25,1.1,1.2,1 --pgam 2.65,2.6,2.6,2.6,2.6 --nshowers 120000 --outputfile corsika_ME.i3.zst
+echo "=================================="
+ls -l
+rm -fr CORSIKA000000.* output* dcors0 corsik*.zst summary.json icecube_icesoft
+echo "=================================="
+echo " Now: `date`"
+echo "=================================="
+/cvmfs/icecube.opensciencegrid.org/py3-v4.2.0/RHEL_7_x86_64/metaprojects/icetray/v1.3.3/env-shell.sh /usr/bin/time /cvmfs/icecube.opensciencegrid.org/py3-v4.2.0/RHEL_7_x86_64/metaprojects/icetray/v1.3.3/simprod-scripts/resources/scripts/corsika5comp.py --corsikaVersion 76900g --CVMFS /cvmfs/icecube.opensciencegrid.org/py3-v4.2.0 --gcdfile /cvmfs/icecube.opensciencegrid.org/data/GCD/GeoCalibDetectorStatus_2020.Run134142.Pass2_V0.i3.gz --UseGSLRNG --UsePipe --compress --CutoffType EnergyPerParticle --eprimarymin=3e4 --eprimarymax=1e6 --pnorm 5,2.25,1.1,1.2,1 --pgam 2.65,2.6,2.6,2.6,2.6 --nshowers 120000 --outputfile corsika_ME.i3.zst
+echo "=================================="
+ls -l
+rm -fr CORSIKA000000.* output* dcors0 corsik*.zst summary.json icecube_icesoft
+echo "=================================="
+echo " Now: `date`"
+echo "=================================="
+/cvmfs/icecube.opensciencegrid.org/py3-v4.2.0/RHEL_7_x86_64/metaprojects/icetray/v1.3.3/env-shell.sh /usr/bin/time /cvmfs/icecube.opensciencegrid.org/py3-v4.2.0/RHEL_7_x86_64/metaprojects/icetray/v1.3.3/simprod-scripts/resources/scripts/corsika5comp.py --corsikaVersion 76900g --CVMFS /cvmfs/icecube.opensciencegrid.org/py3-v4.2.0 --gcdfile /cvmfs/icecube.opensciencegrid.org/data/GCD/GeoCalibDetectorStatus_2020.Run134142.Pass2_V0.i3.gz --UseGSLRNG --UsePipe --compress --CutoffType EnergyPerParticle --eprimarymin=3e4 --eprimarymax=1e6 --pnorm 5,2.25,1.1,1.2,1 --pgam 2.65,2.6,2.6,2.6,2.6 --nshowers 120000 --outputfile corsika_ME.i3.zst
+echo "=================================="
+ls -l
+rm -fr CORSIKA000000.* output* dcors0 corsik*.zst summary.json icecube_icesoft
+echo "=================================="
+echo " Now: `date`"
+echo "=================================="
