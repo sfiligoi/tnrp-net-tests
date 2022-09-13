f3e781ce15317d8b205dc5af1ca2b916f9dba1cd
Igor Sfiligoi <isfiligoi@ucsd.edu>
2022-09-13 09:32:47 -0700

    Add IceCube NRP benchmark

--- nrp_2022/icecube/README.txt
+++ nrp_2022/icecube/README.txt
@@ -1,12 +1,14 @@
 Create configmap
 ================
-kubectl create configmap icecube-nrp-script -n osg-icecube --from-file=run_test.sh
+kubectl create configmap icecube-nrp-script -n osg-icecube --from-file=run_test.sh --from-file=run_benchmark.sh
 
 Run test
 ========
 kubectl create -n osg-icecube -f icecube_nrp.yaml
+# optional
+kubectl create -n osg-icecube -f icecube_nrp_benchmark.yaml
 
 Retrieve results
 ================
-kubectl get pods -n osg-icecube -o wide |grep nrp-test
+kubectl get pods -n osg-icecube -o wide |grep icecube-nrp
 kubectl logs -n osg-icecube <icecube-nrp-*>
---
+++ nrp_2022/icecube/run_benchmark.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+clinfo
+echo "======================================="
+nvidia-smi
+echo "======================================="
+if [ "x${CUDA_VISIBLE_DEVICES}" == "x" ]; then
+  export CUDA_VISIBLE_DEVICES=${NVIDIA_VISIBLE_DEVICES}
+fi
+echo "CUDA_VISIBLE_DEVICES = ${CUDA_VISIBLE_DEVICES}"
+date
+echo "NRP BENCHMARK Start: `date +%s`"
+time /cvmfs/icecube.opensciencegrid.org/py3-v4.1.1/icetray-env combo/V01-00-02 /cvmfs/icecube.opensciencegrid.org/py3-v4.1.1/metaprojects/combo/V01-00-02/clsim/resources/scripts/benchmark.py -n 5000
+echo "NRP BENCHMARK End: `date +%s`"
+date
+echo "NRP BENCHMARK Start: `date +%s`"
+time /cvmfs/icecube.opensciencegrid.org/py3-v4.1.1/icetray-env combo/V01-00-02 /cvmfs/icecube.opensciencegrid.org/py3-v4.1.1/metaprojects/combo/V01-00-02/clsim/resources/scripts/benchmark.py -n 5000
+echo "NRP BENCHMARK End: `date +%s`"
+date
