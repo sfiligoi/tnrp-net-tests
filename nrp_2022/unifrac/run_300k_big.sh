6b878843dcb908e640ac0796c57286aa3b798896
Igor Sfiligoi <isfiligoi@ucsd.edu>
2022-10-22 11:29:43 -0700

    Update run_300k_big.sh

--- nrp_2022/unifrac/run_300k_big.sh
+++ nrp_2022/unifrac/run_300k_big.sh
@@ -10,16 +10,29 @@ wget -q http://uaf-10.t2.ucsd.edu/~sfiligoi/unifrac_inputs/300k/x_merged.withpla
 wget -q http://uaf-10.t2.ucsd.edu/~sfiligoi/unifrac_inputs/300k/archive-redbiom-070920-insertion_tree.relabelled.tre
 export UNIFRAC_GPU_INFO=Y
 export UNIFRAC_TIMING_INFO=Y
-export OMP_NUM_THREADS=16
+export OMP_NUM_THREADS=32
 export ISTART=0
 export IEND=153619
-export ISTEP=19248
-date
-echo "NRP TEST Start: `date +%s`"
-source /root/run_300k_support.sh 
-echo "NRP TEST End: `date +%s`"
+export ISTEP=25664
+
+export ACC_DEVICE_NUM=0
+export ICORES=`nvidia-smi topo -m | grep '^GPU0' |awk '{print $5}'`
 
 date
 echo "NRP TEST Start: `date +%s`"
+t1=`date +%s`
 source /root/run_300k_support.sh 
+t2=`date +%s`
 echo "NRP TEST End: `date +%s`"
+let t3=${t2}-${t1}
+echo "Total runtime: ${t3}"
+
+date
+echo "NRP TEST Start: `date +%s`"
+t1=`date +%s`
+source /root/run_300k_support.sh 
+t2=`date +%s`
+echo "NRP TEST End: `date +%s`"
+let t3=${t2}-${t1}
+echo "Total runtime: ${t3}"
+
