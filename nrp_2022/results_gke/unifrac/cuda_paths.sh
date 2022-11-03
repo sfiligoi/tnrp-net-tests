d1731557a1b5d759cda20a2533d616aa1c27dfb5
Igor Sfiligoi <isfiligoi@sdsc.edu>
2022-11-03 19:43:33 +0000

    Add GKE results

---
+++ nrp_2022/results_gke/unifrac/cuda_paths.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# starting with cuda-11, the nvidia/cuda bins and libs are not in he defaul pahs anymore
+# create symlinks as a short-term fix to ensure backward compatibilitty
+
+for d in /usr/local/nvidia/bin 
+do
+  pushd $d
+  for f in *; do
+    ln -s $d/$f /bin/$f
+  done
+  popd
+done
+
+for d in /usr/local/nvidia/lib64 /usr/local/cuda/lib64
+do
+  pushd $d
+  for f in *so*; do
+    ln -s $d/$f /usr/lib/$f
+  done
+  popd
+done
+
